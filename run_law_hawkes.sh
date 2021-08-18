173f7459c5c4bdd00a40776eccaa8a3d91d32c82
Yamac <yamac619@gmail.com>
2021-08-18 08:48:16 -0400

    Results file for tracking

--- run_law_hawkes.sh
+++ run_law_hawkes.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 device=0
 data=data/simulated/power_hawkes/
-batch=1
+batch=16
 n_head=1
 n_layers=1
-d_model=16
-d_rnn=32
-d_inner=32
-d_k=8
-d_v=8
+d_model=64
+d_rnn=64
+d_inner=128
+d_k=16
+d_v=16
 dropout=0.1
 lr=1e-4
 smooth=0.1
 epoch=50
 log=log.txt
 
-python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model $d_model -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -smooth $smooth -epoch $epoch -log $log -save True
+python Main.py -data $data -batch 1 -n_head $n_head -n_layers $n_layers -d_model $d_model -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -smooth $smooth -epoch $epoch -log $log -save True
+python Main.py -data $data -batch 1 -n_head 3 -n_layers 3 -d_model 64 -d_rnn 64 -d_inner 256 -d_k 16 -d_v 16 -dropout $dropout -lr $lr -smooth $smooth -epoch $epoch -log $log -save True
 
