b9c908149b922cc02fe4af71c8d922f7584a95f7
Yamac <yamac619@gmail.com>
2022-01-17 23:22:14 -0500

    Results file for tracking

--- run_mimic.sh
+++ run_mimic.sh
@@ -12,8 +12,11 @@ d_v=16
 dropout=0.1
 lr=5e-4
 smooth=0.1
-epoch=100
+epoch=2
 log=log.txt
 
-python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model 64 -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr 0.0005 -smooth $smooth -epoch 100 -log $log -save True
+python Main.py -data $data -batch $batch -n_head 4 -n_layers 4 -d_model 64 -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr 0.0005 -smooth $smooth -epoch 100 -log $log -save True
+python Main.py -data $data -batch $batch -n_head 1 -n_layers 1 -d_model 64 -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr 0.0005 -smooth $smooth -epoch 100 -log $log -save True
 
+python Main.py -data $data -batch $batch -n_head 4 -n_layers 4 -d_model 64 -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr 0.0005 -smooth $smooth -epoch 250 -log $log -save True
+python Main.py -data $data -batch $batch -n_head 1 -n_layers 1 -d_model 64 -d_rnn $d_rnn -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr 0.0005 -smooth $smooth -epoch 250 -log $log -save True
